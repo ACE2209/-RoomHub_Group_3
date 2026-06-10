95b4e1db08dab376d441bcbb82101dd123dd7142
Truong Tran <trantnce180829@fpt.edu.vn>
2026-06-10 14:04:58 +0700

    fix(router): resolve routing issues

--- FE_RoomHub/src/routes/AppRoutes.js
+++ FE_RoomHub/src/routes/AppRoutes.js
@@ -7,45 +7,37 @@ import Login from "../pages/auth/Login";
 import Register from "../pages/auth/Register";
 import ForgotPassword from "../pages/auth/ForgotPassword";
 import ResetPassword from "../pages/auth/ResetPassword";
+import ChangePassword from "../pages/auth/ChangePassword";
+import Profile from "../pages/profile/Profile";
 import ReviewManagementPage from "../pages/admin/ReviewManagementPage";
 import ReviewDetailPage from "../pages/admin/ReviewDetailPage";
 import HomePage from "../pages/HomePage";
 import AdminDashboardPage from "../pages/admin/AdminDashboardPage";
+
+import ReportManagementPage from "../pages/admin/ReportManagementPage";
+import ReportDetailPage from "../pages/admin/ReportDetailPage";
 
 
 const AppRoutes = () => {
     return (
         <Routes>
 
-            <Route
-                path="/"
-                element={
-                    <HomePage />
-                }
-            />
+            <Route path="/" element={<HomePage />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
 
-            <Route
-                path="/login"
-                element={<Login />}
-            />
+            <Route path="/forgot-password" element={<ForgotPassword />} />
+            <Route path="/reset-password/:token" element={<ResetPassword />} />
+            <Route path="/change-password" element={<ChangePassword />} />
 
-            <Route
-                path="/register"
-                element={<Register />}
-            />
+            <Route path="/admin/reviews" element={<ReviewManagementPage />} />
+            <Route path="/admin/reviews/:reviewId" element={<ReviewDetailPage />} />
+            <Route path="/admin" element={<AdminDashboardPage />} />
 
-            <Route
-                path="/forgot-password"
-                element={<ForgotPassword />}
-            />
+            <Route path="/profile" element={<Profile />} />
 
-            <Route
-                path="/reset-password/:token"
-                element={<ResetPassword />}
-            />
-<Route path="/admin/reviews" element={<ReviewManagementPage />} />
-      <Route path="/admin/reviews/:reviewId" element={<ReviewDetailPage />} />
-        <Route path="/admin" element={<AdminDashboardPage />} />
+            <Route path="/admin/reports" element={<ReportManagementPage />} />
+            <Route path="/admin/reports/:reportId" element={<ReportDetailPage />} />
         </Routes >
 
     );
